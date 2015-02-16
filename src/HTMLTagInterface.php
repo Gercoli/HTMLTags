216e1047731c51f1b796a613acc17c10c08a2474
Garry Ercoli <Garry@GErcoli.com>
2015-02-16 01:20:46 -0600

    Added an interface to better map the class

---
+++ src/HTMLTagInterface.php
@@ -0,0 +1,38 @@
+<?php namespace GErcoli\HTMLTags;
+
+
+interface HTMLTagInterface {
+
+    public function setAttribute($name,$value);
+    public function getAttribute($name);
+    public function removeAttribute($name);
+    public function hasAttribute($name);
+
+    public function addClass($className);
+    public function removeClass($className);
+    public function hasClass($className);
+    public function getClasses();
+    public function clearClasses();
+
+    public function appendContent($content);
+    public function prependContent($content);
+    public function clearContent();
+    public function getContent();
+
+    public function setClosingTag($enable);
+    public function getClosingTag();
+
+    public function setXHTMLEncoding($enable);
+    public function getXHTMLEncoding();
+
+    public function getFormattedAttributes();
+    public function getTagType();
+
+    public function setTagPrefix($prefix);
+    public function getTagPrefix();
+    public function getPreviousTagPrefix();
+
+    public function __toString();
+
+
+}
